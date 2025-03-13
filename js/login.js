9fdc9681b08431933ca9d89887cc40d9154389ac
NicolasMp3 <164546658+NicolasMp3@users.noreply.github.com>
2025-03-13 15:50:56 +0000

    final

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 // Seleciona o botão de login pelo ID e adiciona um evento de clique para chamar a função autenticar
-document.querySelector('#login-form').addEventListener('submit', autenticar);
+const botaoLogin = document.querySelector('#login-form');
+botaoLogin.addEventListener('submit', autenticar);
 
 // Seleciona a área onde as mensagens de status serão exibidas
 const areaMensagem = document.getElementById('msg');
@@ -8,19 +9,25 @@ const areaMensagem = document.getElementById('msg');
 async function autenticar(e) {
     e.preventDefault(); // Impede que o formulário recarregue a página
 
-    areaMensagem.innerText = "Aguarde... ";
+    document.getElementById('msg').innerText = "Aguarde... ";
 
     const dados = {
         email: document.getElementById('email').value,
         senha: document.getElementById('senha').value
     };
 
+    const dados = {
+    email: document.getElementById('id_do_input_do_email').value,
+    senha: document.getElementById('id_do_input_do_senha').value
+  };
+    
     const url = "https://24-api-a-two.vercel.app/login";
 
     try {
         const response = await fetch(url, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 
+                'Content-Type': 'application/json' },
             body: JSON.stringify(dados)
         });
 
@@ -32,11 +39,11 @@ async function autenticar(e) {
 
         localStorage.setItem('jwt', data.token);
 
-        areaMensagem.style.color = "green";
+        areaMensagem.style = "color:green";
         areaMensagem.innerHTML = "Usuário Autenticado com Sucesso! <br> Token: " + data.token;
 
     } catch (error) {
         areaMensagem.style.color = "red";
-        areaMensagem.innerText = error.message;
+        areaMensagem.innerText = error;
     }
 }
